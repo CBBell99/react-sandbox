4e4679d3a0608dd9e37b2e4d04872b66e36ed738
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-14 23:49:45 -0700

    fix memory leak issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import UseRefExample2 from "./components/UseRefExample2"
+import UseRefExample3 from "./components/UseRefExample3"
 
 function App() {
   return (
     <div className="container mt-5">
-      <UseRefExample2 />
+      <UseRefExample3 />
     </div>
   );
 }
